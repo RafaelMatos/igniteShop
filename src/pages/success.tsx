eeef8669186332ba054c1cf43ecc2b8677f17c9d
RafaelMatos <rafael-matos@outlook.com>
2023-04-21 18:36:25 -0300

    feat(success):sending product images to success page

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,9 @@
 import Link from "next/link";
-import { ImageContainer, SuccessContainer } from "../styles/pages/success";
+import {
+  ImageContainer,
+  ImagesContainer,
+  SuccessContainer,
+} from "../styles/pages/success";
 import { GetServerSideProps } from "next";
 import { stripe } from "../lib/stripe";
 import Stripe from "stripe";
@@ -8,34 +12,39 @@ import Head from "next/head";
 
 interface SuccessProps {
   customerName: string;
-  product: {
-    name: string;
-    imageUrl: string;
-  };
+  productsImages: string[];
 }
 
-export default function Success({ customerName, product }: SuccessProps) {
+export default function Success({
+  customerName,
+  productsImages,
+}: SuccessProps) {
   return (
     <>
       <Head>
-        <link rel="icon" type="image/png" sizes="32x32" href="/logo.png"/>
-        <title>{product.name} | Ignite Shop </title>
-        <meta name="robots" content="noindex"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/logo.png" />
+        <title>Compra finalizada | Ignite Shop </title>
+        <meta name="robots" content="noindex" />
       </Head>
 
       <SuccessContainer>
-        <h1>Compra efetuada</h1>
-        <ImageContainer>
-          <Image
-            src={product.imageUrl}
-            width={120}
-            height={110}
-            alt="Camisa Ignite"
-          />
-        </ImageContainer>
+        <h1>Compra efetuada!</h1>
+        <ImagesContainer>
+          {productsImages.map((image, i) => {
+           return <ImageContainer key={image}>
+              <Image
+                src={image}
+                width={120}
+                height={110}
+                alt="Camisa Ignite"
+              />
+            </ImageContainer>
+          })}
+        </ImagesContainer>
+
         <p>
-          Uhuul <strong>{customerName}</strong>, sua{" "}
-          <strong>{product.name}</strong> já está a caminho da sua casa.
+          Uhuul <strong>{customerName}</strong>, sua compra de 
+          <strong> {productsImages.length}</strong> camisetas já está a caminho da sua casa.
         </p>
         <Link href={"/"}>Voltar ao catálogo</Link>
       </SuccessContainer>
@@ -57,15 +66,17 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   const session = await stripe.checkout.sessions.retrieve(sessionId, {
     expand: ["line_items", "line_items.data.price.product"],
   });
+  // console.log("session.line_items:" ,session.line_items)
   const customerName = session.customer_details?.name;
   const product = session.line_items?.data[0].price?.product as Stripe.Product;
+  const productsImages = session.line_items?.data.map((item) => {
+    const product = item.price?.product as Stripe.Product;
+    return product.images[0];
+  });
   return {
     props: {
       customerName,
-      product: {
-        name: product.name,
-        imageUrl: product.images[0],
-      },
+      productsImages,
     },
   };
 };
